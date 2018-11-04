145ba1fff620f903012567f2fba515916d1dc598
TanguyScholtes <soultaker720@hotmail.com>
2018-11-04 09:08:25 +0100

    finished serie 4 (dates)

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -10,9 +10,20 @@
 // You will have time to focus on it later.
 
 (function() {
-
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-
     // your code here
 
+    function getDate () {
+        var options = {
+            weekday: "long",
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit"
+        };
+        return new Date().toLocaleDateString( "fr-BE", options );
+    }
+
+    document.getElementById( "target" ).innerHTML = getDate();
 })();
--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -10,7 +10,38 @@
 // You will have time to focus on it later.
 
 (function() {
-
     // your code here
+    var year = document.getElementById( "year" ).value;
+    var months = [
+        "Janvier",
+        "Février",
+        "Mars",
+        "Avril",
+        "Mai",
+        "Juin",
+        "Juillet",
+        "Août",
+        "Septembre",
+        "Octobre",
+        "Novembre",
+        "Décembre"
+    ];
 
+    function getFridaysThirteen () {
+        var monthsWithFridayThirteen = "";
+        var date = new Date();
+
+        for ( var month = 0; month <= 11; month++ ) {
+            date.setFullYear( year, month, 13 );
+
+            if ( date.getDay() == 5 ) {
+                //if the day is friday (0 being sunday, 1 monday, etc..., so 5 is friday)
+                monthsWithFridayThirteen += months[ month ] + ", ";
+            }
+        }
+
+        alert( monthsWithFridayThirteen );
+    }
+
+    document.getElementById( "run" ).addEventListener( "click", getFridaysThirteen, false );
 })();
