87ed9e765a9224a29972ef3cea1a13cd93dba0ce
TanguyScholtes <soultaker720@hotmail.com>
2018-11-06 09:40:55 +0100

    finished serie 7 (misc) ex 6 (guess-number)

--- 07-misc/06-guess-number/script.js
+++ 07-misc/06-guess-number/script.js
@@ -10,7 +10,31 @@
 // You will have time to focus on it later.
 
 (function() {
-
     // your code here
+    function rng ( min, max ) {
+        return Math.floor( Math.random() * ( max - min ) ) + min;
+    }
 
+    var tries = 0;
+    var min = 1;
+    var max = 100;
+    var toGuess = rng( 1, 100 );
+
+    function ask () {
+        var guess = prompt( "Devinez le nombre entre " + min + " et " + max + ".\n Essais : " + tries );
+        if ( guess == toGuess ) {
+            tries++;
+            alert( "Félicitations ! Vous avez deviné le nombre caché.\n Nombre : " + toGuess + "\n Essais : " + tries );
+        } else if ( guess < toGuess ) {
+            tries++;
+            alert( "Raté. Le nombre caché est plus grand que ça !" );
+            ask();
+        } else {
+            tries++;
+            alert( "Hé non. Le nombre caché est plus petit." );
+            ask();
+        }
+    }
+
+    ask();
 })();
